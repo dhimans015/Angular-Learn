c91b0cc35cfdcb9a23f9e8c744ae54f7731abcf6
Saurabh Kumar <dhimans015@gmail.com>
2022-04-10 12:14:11 +0530

    added alert component.

---
+++ src/app/Warning_Alert/warning_alert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'warning_alert',
+    templateUrl:'./warning_alert.component.html'
+})
+export class warning_alert{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ServerComponent } from './Server/server.component';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { warning_alert } from './Warning_Alert/warning_alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    warning_alert
   ],
   imports: [
     BrowserModule
