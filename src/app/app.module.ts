714f21d27ded6a6822c903da0ac048443ec3f441
Saurabh Kumar <dhimans015@gmail.com>
2022-04-10 11:40:01 +0530

    first-commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServerComponent } from './Server/server.component';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
